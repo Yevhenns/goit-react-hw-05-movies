b732621bc39058d2354cc6a083812c2e04d3df99
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-27 12:51:44 +0200

    new files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { MovieDetails } from 'pages/MovieDetails';
+import { Cast } from './Cast';
+import { Reviews } from './Reviews';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:movieId" element={<MovieDetails />}>
+        <Route path="cast" element={<Cast />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 };
