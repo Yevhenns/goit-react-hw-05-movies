da7c51128ce0964a337e79cd7bce3585828f84a1
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-27 10:06:58 +0200

    route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from 'react';
+import { Routes, Route } from 'react-router-dom';
 export const App = () => {
   return (
     <Routes>
-      <Route path='/'/>
+      <Route path="/" />
     </Routes>
   );
 };
