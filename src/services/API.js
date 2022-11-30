44f46d7e1559ea853b920adade173c99b1252e82
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-30 18:56:35 +0200

    Update API.js

--- src/services/API.js
+++ src/services/API.js
@@ -3,4 +3,4 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'f03787ea9d18175277e6339ec6fd6212';
 
-export async function frtchMovies() {}
+export async function fetchMovies() {}
