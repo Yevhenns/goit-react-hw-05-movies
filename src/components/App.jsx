acf7325fd84fabab3905129cbf3e707996f294c0
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-30 18:56:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,11 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />}>
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
         {/* <Route path="*" element={<Layout />} /> */}
       </Route>
