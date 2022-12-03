484f95ae041cfd0448c6f17170e6d8f8a24b2f80
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-03 19:30:19 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ export const Home = () => {
     fetchTrending()
       .then(console.log(fetchTrending()))
       .catch(error => console.log(error.message))
-      .finally(() => console.log(2));
+        .finally(() => console.log(2));
+    return (<h1>Trending today</h1>)
 }
   
-
 
 
 
