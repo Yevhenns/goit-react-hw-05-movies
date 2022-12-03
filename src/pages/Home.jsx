8f2d9aa0115143352ec46acf5644ae7be9498d7a
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-03 18:13:07 +0200

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,13 @@
-export const Home = () => {}
+import { fetchTrending } from 'services/API';
+
+export const Home = () => {
+    fetchTrending()
+      .then(console.log(fetchTrending()))
+      .catch(error => console.log(error.message))
+      .finally(() => console.log(2));
+}
+  
+
+
+
+
--- src/services/API.js
+++ src/services/API.js
@@ -2,5 +2,18 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'f03787ea9d18175277e6339ec6fd6212';
+const params = {
+  api_key: API_KEY,
+  page: 1,
+};
 
-export async function fetchMovies() {}
+export const fetchTrending = async () => {
+  try {
+    const responce = await axios('/trending/movie/day', { params });
+    return responce.data.results;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
