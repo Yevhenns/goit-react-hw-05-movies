cf6a194c084efa581c3526aa7e8d8bc521eee79a
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-04 09:50:12 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
 export const Movies = () => {
-    return (<>
-        <input></input>
-    <button type="submit">Search</button></>)
-}
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = e => {
+    setQuery(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (query.trim() === '') {
+      alert('Empty request');
+      return;
+    }
+    // onSubmit(query);
+    setQuery('');
+  };
+
+  return (
+    <>
+      <input value={query} onChange={handleQueryChange}></input>
+      <button type="submit" onClick={handleSubmit}>
+        Search
+      </button>
+    </>
+  );
+};
