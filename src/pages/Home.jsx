361bf3666d3fd83d26f8aad080c70807df87cff8
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-04 12:00:35 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchTrending } from 'services/API';
 
 export const Home = () => {
   const [trendMovies, setTrendMovies] = useState([]);
 
   useEffect(() => {
-    fetchTrending().then(res => setTrendMovies(res));
+    fetchTrending().then(setTrendMovies);
   }, []);
 
   return (
@@ -13,7 +14,9 @@ export const Home = () => {
       <h1>Trending today</h1>
       <ul>
         {trendMovies.map(trendMovie => (
-          <li key={trendMovie.id}>{trendMovie.title}</li>
+          <li key={trendMovie.id}>
+            <Link to={trendMovie.id}>{trendMovie.title}</Link>
+          </li>
         ))}
       </ul>
     </>
