b7a99eb4c7f989f14543841cf8f2c609a1ee6bb1
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-17 20:45:50 +0200

    mov

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/:movieId" element={<MovieDetails />}>
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,1 +1,85 @@
-export const MovieDetails = () => {};
+import { Suspense, useEffect, useState } from 'react';
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
+import { getMovieById } from 'services/API';
+
+// import styles from './MovieItem.module.css';
+// import { Spinner } from 'components/Spinner/Spinner';
+
+export const MovieDetails = () => {
+  const [movieData, setMovieData] = useState(null);
+  const [error, setError] = useState(null);
+  const { movieId } = useParams();
+  const location = useLocation();
+  const backLink = location?.state?.from ?? '/';
+
+  useEffect(() => {
+    try {
+      getMovieById(movieId, false, false).then(data => {
+        const { original_title, overview, poster_path, genres, popularity } =
+          data;
+        setMovieData({
+          original_title,
+          overview,
+          poster_path,
+          popularity,
+          genres: genres.map(genre => genre.name),
+        });
+      });
+    } catch (error) {
+      setError(error);
+    }
+  }, [movieId]);
+
+  return (
+    <div>
+      {movieData && (
+        <>
+          <Link to={backLink}>
+            Go back
+          </Link>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w400${movieData.poster_path}`}
+              alt={movieData.original_title}
+              width="200"
+            />
+            <div>
+              <h2>{movieData.original_title}</h2>
+              <p>User Score: {movieData.popularity}%</p>
+              <h3>Overview</h3>
+              <p>{movieData.overview}</p>
+              <h3>Genres</h3>
+
+              <p>{movieData.genres.join(' ')}</p>
+            </div>
+          </div>
+
+          <ul>
+            <span>Additional infromation</span>
+            <li>
+              <NavLink to="cast" state={{ from: backLink }}>
+                Cast
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="reviews" state={{ from: backLink }}>
+                Reviews
+              </NavLink>
+            </li>
+          </ul>
+
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </>
+      )}
+      {error && error.message}
+    </div>
+  );
+};
